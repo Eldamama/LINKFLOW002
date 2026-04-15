347b3e0ca9154db58fe009be0fdfa9decf0911dd
Eldamama <134916806+Eldamama@users.noreply.github.com>
2026-04-15 16:08:04 +0100

    Create firebase-config.js for Firebase setup
    Add Firebase configuration and initialization code.

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// Importations via CDN pour que GitHub Pages puisse les lire
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+
+// Votre configuration Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDgBD9NrGaUU92l7vBadVyENH_rby8zZ-w",
+  authDomain: "linkflow-7a82a.firebaseapp.com",
+  databaseURL: "https://linkflow-7a82a-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "linkflow-7a82a",
+  storageBucket: "linkflow-7a82a.firebasestorage.app",
+  messagingSenderId: "459654757296",
+  appId: "1:459654757296:web:d6e2609d19d3d7f4a7b475",
+  measurementId: "G-THW9XMEJTX"
+};
+
+// Initialisation de Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Exportation pour l'utiliser dans vos autres fichiers
+export { app };
